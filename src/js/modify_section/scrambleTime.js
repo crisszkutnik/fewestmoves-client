3d97c889b850ed6876e1c9e3614cefc060e28d3f
cristobalszk <crisszkutnik@gmail.com>
2020-08-14 13:04:35 -0300

    Added button to show/hide time

--- src/js/modify_section/scrambleTime.js
+++ src/js/modify_section/scrambleTime.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
 class ScrambleTime extends React.Component {
+   constructor(props) {
+      super(props);
+
+      this.state = {
+         opacity: 1
+      }
+
+      this.parseNumber = this.parseNumber.bind(this);
+      this.changeTimeVisibility = this.changeTimeVisibility.bind(this);
+   }
+
    parseNumber(num) {
       return (num >= 10 ? num : '0' + num)
+   }
+
+   changeTimeVisibility() {
+      this.setState({
+         opacity: this.state.opacity === 1 ? 0 : 1
+      });
    }
 
    componentWillUnmount() {
@@ -13,8 +30,9 @@ class ScrambleTime extends React.Component {
       return (
          <>
          <p>{this.props.scramble}</p>
-         <h1>{this.parseNumber(this.props.minutes)}:{this.parseNumber(this.props.seconds)}</h1>
-         <span>minutes left</span>
+         <h1 style={{opacity: this.state.opacity}}>{this.parseNumber(this.props.minutes)}:{this.parseNumber(this.props.seconds)}</h1>
+         <span style={{opacity: this.state.opacity}}>minutes left</span>
+         <button onClick={this.changeTimeVisibility}>{this.state.opacity === 1 ? "Hide time" : "Show time"}</button>
          </>
       );
    }
